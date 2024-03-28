4de6127fb85795980dc5ba29ae7836672f612cc4
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2024-03-28 09:52:09 +0000

    test: stricter assertions on booking IDs

--- tests/api/get-bookings.spec.js
+++ tests/api/get-bookings.spec.js
@@ -1,11 +1,14 @@
 const { test, expect } = require('@playwright/test');
 
 test.describe('GET /booking', () => {
-  test.only('should return all the existing booking IDs', async ({
-    request,
-  }) => {
+  test('should return all the existing booking IDs', async ({ request }) => {
     const res = await request.get('/booking');
     expect(res.ok()).toBeTruthy();
-    expect(await res.json()).toHaveLength(10);
+
+    const bookingIDs = await res.json();
+    expect(bookingIDs).toHaveLength(10);
+    bookingIDs.forEach((bookingID) => {
+      expect(bookingID).toMatchObject({ bookingid: expect.any(Number) });
+    });
   });
 });
